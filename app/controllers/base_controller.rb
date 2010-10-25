9d034265009476627be18a7983ae5a663008a286
Nick Kim <ksh1204@hotmail.com>
2010-10-24 19:58:48 -0700

    email activation fixed

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,6 +2,10 @@ class BaseController < ApplicationController
 
   def index
   end
+  
+  def all_users
+    @users = User.all
+  end
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101022024315) do
+ActiveRecord::Schema.define(:version => 20101025025302) do
+
+  create_table "messages", :force => true do |t|
+    t.integer  "sender_id"
+    t.integer  "receiver_id"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_comments", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_ratings", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
