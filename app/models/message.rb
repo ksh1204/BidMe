41ee4d92706e73394d1f4f5e79f4f246827ceea3
Nick Kim <ksh1204@hotmail.com>
2010-10-27 00:24:56 -0700

    Cleaning up unused files

--- app/models/message.rb
+++
@@ -1,4 +0,0 @@
-class Message < ActiveRecord::Base
-  belongs_to :sender, :class_name => "User"
-  belongs_to :receiver, :class_name => "User"
-end
--- vendor/plugins/growl4rails/init.rb
+++
@@ -1,2 +0,0 @@
-require "#{File.dirname __FILE__}/lib/growl"
-ActionView::Base.send(:include, ActionView::Helpers::Growl4RailsHelper)
